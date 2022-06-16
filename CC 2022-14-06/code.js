f7a59d707f8152c02cf94c0c38c574019e765377
junejanean <junejanean@gmail.com>
2022-06-16 16:45:23 -0600

    codewar solution 6-16

--- CC 2022-14-06/code.js
+++ CC 2022-14-06/code.js
@@ -1,16 +1,18 @@
-//Code as fast as you can! You need to double the integer and return it.
+//Given a non-empty array of integers, return the result of multiplying the values together in order.
 
 // Parameters
-// an integer
-//double it ( * 2)
+// an array with integers
+// multiply each integer in their order
 
 // Return
 // one integer
 
 // Examples
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
 // Pseudo Code
 
-function doubleInteger(i) {
-	return i * 2;
+function grow(x) {
+	return x.reduce((a, b) => a * b);
 }
+grow([4, 1, 1, 1, 4]);
