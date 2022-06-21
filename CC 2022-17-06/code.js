b36d8fa1c0ca230b9c727f0165b318d4674f6939
junejanean <junejanean@gmail.com>
2022-06-20 18:47:43 -0600

    codewars solution

--- CC 2022-17-06/code.js
+++ CC 2022-17-06/code.js
@@ -1,36 +1,17 @@
-//Given a list of integers,
-// determine whether the sum of its elements is odd or even.
+//We need a function that can transform a number into a string.
 
 // Parameters
-// an array of integers negative or positive
-// sum all integers together
+// a number
 
 // Return
-// if an odd number, return a string "odd"
-// if an even number, return a string "even"
-// if 0 or empty, return even
+// a number as a string
 
 // Examples
-// Input: [0]
-// Output: "even"
-
-// Input: [0, 1, 4]
-// Output: "odd"
-
-// Input: [0, -1, -5]
-// Output: "even"
+// 123 --> "123"
+// 999 --> "999"
 
 // Pseudo Code
-
-function oddOrEven(array) {
-	let sum = 0;
-	for (var i = 0; i < array.length; i++) {
-		sum += array[i];
-	}
-	if (sum % 2 === 0) {
-		return 'even';
-	} else {
-		return 'odd';
-	}
+function numberToString(num) {
+	return num.toString();
 }
-oddOrEven([0, 2, 5]);
+numberToString(67);
