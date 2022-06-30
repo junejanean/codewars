fe37655afb59b526e4cc86530a566df9fabfeda4
junejanean <junejanean@gmail.com>
2022-06-29 18:42:27 -0600

    codewars solution

---
+++ 2022-06/CC 2022-29-06/code.js
@@ -0,0 +1,18 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+// Parameters
+// a string
+
+// Return
+// a string 'hello world'
+// style and output as clever as possible
+
+// Examples
+
+// Pseudo Code
+
+function greet() {
+	return 'hello world!';
+}
+
+const greet = () => 'hello world!';
