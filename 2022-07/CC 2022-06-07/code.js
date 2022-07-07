830f466a7a75d03f8b70b7b91d5a81ae15f4f990
junejanean <junejanean@gmail.com>
2022-07-06 20:31:18 -0600

    codewars solution

---
+++ 2022-07/CC 2022-06-07/code.js
@@ -0,0 +1,22 @@
+// Merge two sorted arrays into one
+// You are given two sorted arrays that both only contain integers.
+// Your task is to find a way to merge them into a single one, sorted in asc order.
+// Complete the function mergeArrays(arr1, arr2), where arr1 and arr2
+// are the original sorted arrays.
+
+// Parameters
+// two arrays
+
+// Return
+// an array with the elements without any repeats
+
+// Examples
+// [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Pseudo Code
+
+function mergeArrays(arr1, arr2) {
+	return Array.from(new Set(arr1.concat(arr2))).sort((a, b) => a - b);
+}
+mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]);
+mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]);
